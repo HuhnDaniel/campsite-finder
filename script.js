0afa43df7270ed29e1ef4af5ff3754be4fe9fd66
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-04-21 19:37:16 -0500

    added HTTPS prefilter

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+// Prefilter to allow access to protected HTTPS urls
+// In ajax calls, add parameter crossDomain: true to enable
+jQuery.ajaxPrefilter(function(options) {
+	if (options.crossDomain && jQuery.support.cors) {
+		options.url = "https://cors-anywhere.herokuapp.com/" + options.url;
+	}
+});
