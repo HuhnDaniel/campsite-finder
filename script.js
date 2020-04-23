e7d1226954d0bcd20235f2876637d8d09e419849
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-04-22 22:08:35 -0500

    all campsites w/ placeholders

--- script.js
+++ script.js
@@ -9,29 +9,44 @@ jQuery.ajaxPrefilter(function(options) {
 });
 
 $(document).ready(function() {
+
+	// Replace state with query selector for dropdown
 	var state = "KS";
+
+	// Replace placeholderBtn with submit button ID
 	$("#placeholderBtn").on("click", function(e) {
 		e.preventDefault();
-		searchState(state);
+		searchState(state, 0);
 	});
 });
 
 // Function to search campsites in a specific state
-function searchState(state) {
+function searchState(state, offset) {
 	$.ajax({
-		url: "https://ridb.recreation.gov/api/v1/facilities?state=" + state + "&full=true&apikey=" + apiKey,
+		url: "https://ridb.recreation.gov/api/v1/facilities?state=" + state + "&offset=" + offset + "&full=true&apikey=" + apiKey,
 		method: "GET",
 		crossDomain: true
 	}).then(function(facilities) {
 		console.log(facilities);
+		var rec = facilities.RECDATA;
+		var meta = facilities.METADATA;
 		
 		// Filter through facilities looking for only campsites
-		var campsites = [];
-		for (var i = 0; i < facilities.RECDATA.length; i++) {
-			if (facilities.RECDATA[i].FacilityTypeDescription === "Campground") {
-				$("#placeholder-div").append($("<article>").addClass("put-classes-here").attr("data-facilityID", facilities.RECDATA[i].FacilityID).text(facilities.RECDATA[i].FacilityName));
+		for (var i = 0; i < rec.length; i++) {
+			if (rec[i].FacilityTypeDescription === "Campground") {
+
+				// Replace placeholder-div with list target div
+				$("#placeholder-div").append($("<article>").addClass("put-classes-here")
+														   .attr("data-facilityID", rec[i].FacilityID)
+														   .text(rec[i].FacilityName));
 			}
 		}
-		console.log(campsites);
+		console.log(meta.RESULTS.CURRENT_COUNT, meta.RESULTS.TOTAL_COUNT);
+
+		// Check if all results are avalible, if not, print more
+		if ((meta.RESULTS.CURRENT_COUNT + offset) < meta.RESULTS.TOTAL_COUNT) {
+			offset += meta.SEARCH_PARAMETERS.LIMIT;
+			searchState(state, offset);
+		}
 	});
 }
