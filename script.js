8c24cf491bfbb1fee9e2e53496ae049c04d8f168
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-04-22 21:49:39 -0500

    print campsites in state

--- script.js
+++ script.js
@@ -8,8 +8,15 @@ jQuery.ajaxPrefilter(function(options) {
 	}
 });
 
+$(document).ready(function() {
+	var state = "KS";
+	$("#placeholderBtn").on("click", function(e) {
+		e.preventDefault();
+		searchState(state);
+	});
+});
+
 // Function to search campsites in a specific state
-var state = "KS";
 function searchState(state) {
 	$.ajax({
 		url: "https://ridb.recreation.gov/api/v1/facilities?state=" + state + "&full=true&apikey=" + apiKey,
@@ -17,16 +24,14 @@ function searchState(state) {
 		crossDomain: true
 	}).then(function(facilities) {
 		console.log(facilities);
-
+		
 		// Filter through facilities looking for only campsites
 		var campsites = [];
 		for (var i = 0; i < facilities.RECDATA.length; i++) {
 			if (facilities.RECDATA[i].FacilityTypeDescription === "Campground") {
-				campsites.push(facilities.RECDATA[i]);
+				$("#placeholder-div").append($("<article>").addClass("put-classes-here").attr("data-facilityID", facilities.RECDATA[i].FacilityID).text(facilities.RECDATA[i].FacilityName));
 			}
 		}
 		console.log(campsites);
 	});
 }
-
-searchState(state);
